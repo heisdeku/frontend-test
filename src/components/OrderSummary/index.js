352c52475d7cf72c62b019e256833f8bef4da7e9
Oluwaferanmi Adedokun <adedokunoluwaferanmi@gmail.com>
2021-03-18 00:02:30 +0100

    feat: order summary component

---
+++ src/components/OrderSummary/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import {
+  getAllProductsTotal,
+  getAllProductsTotalWithTax,
+} from '../../services/helpers'
+
+import './OrderSummary.scss'
+const OrderSummarySection = ({ orders }) => {
+  return (
+    <div className='order-summary'>
+      <div className='flex'>
+        <span>Total Items Ordered:</span>
+        <p> {orders.length} Items</p>
+      </div>
+      <div className='flex'>
+        <span>Total price:</span>
+        <p> {`${orders[0].currency} ${getAllProductsTotal(orders)}`}</p>
+      </div>
+      <div className='flex'>
+        <span>Total price (inc. Tax):</span>
+        <p> {`${orders[0].currency} ${getAllProductsTotalWithTax(orders)}`}</p>
+      </div>
+    </div>
+  )
+}
+
+export default OrderSummarySection
